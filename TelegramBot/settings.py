5d134c16edb571088413f23a4d9b9562d8de3aa2
nursultanamanzholdev <nursultanamanzholdev@gmail.com>
2025-02-11 15:35:21 +0500

    Update settings v2

--- TelegramBot/settings.py
+++ TelegramBot/settings.py
@@ -123,8 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRONJOBS = [
-    # This cron expression means "At minute 0 of every hour"
-    ('0 * * * *', 'django.core.management.call_command', ['check_answers']),
-]
